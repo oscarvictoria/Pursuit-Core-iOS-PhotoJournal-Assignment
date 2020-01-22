25b0c113bc2df73c7c3b701cf529d213a3a56dad
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-01-22 15:38:40 -0500

    added project

---
+++ PhotoJournal/PhotoJournal/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PhotoJournal
+//
+//  Created by Oscar Victoria Gonzalez  on 1/22/20.
+//  Copyright © 2020 Oscar Victoria Gonzalez . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
