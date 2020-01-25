e35a7dd913bf46311b9d98b85e0ac5ec5d3d7e8f
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-01-25 12:57:13 -0500

    updated image view

--- PhotoJournal/PhotoJournal/Controllers/PostViewController.swift
+++ PhotoJournal/PhotoJournal/Controllers/PostViewController.swift
@@ -11,7 +11,11 @@ import AVFoundation
 
 class PostViewController: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    
+    @IBOutlet weak var photoLibraryImage: UIImageView!
     
     var imageObjects = [ImageObject]()
     
@@ -20,18 +24,19 @@ class PostViewController: UIViewController {
     let dataPersistance = PersistenceHelper(filename: "images.plist")
     
     
-    private var selectedImage: UIImage? {
-        didSet {
-            appendNewPhotoToCollection()
-        }
-    }
+//    private var selectedImage: UIImage? {
+//        didSet {
+//            appendNewPhotoToCollection()
+//        }
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.dataSource = self
-        tableView.delegate = self
         imagePickerController.delegate = self
-//        loadImageObjects()
+        textView.delegate = self
+        textView.text = "Enter photo description here"
+        textView.textColor = UIColor.lightGray
+        //        loadImageObjects()
         
     }
     
@@ -43,49 +48,52 @@ class PostViewController: UIViewController {
         }
     }
     
-    func appendNewPhotoToCollection() {
-        guard let image = selectedImage else {
-            print("image is nil")
-            return
-        }
-        
-        print("original image size is \(image.size)")
-        
-        // the size for resizing of the image
-//        let size = UIScreen.main.bounds.size
-//        // we will maintain the aspect ratio of the image
-//        let rec = AVMakeRect(aspectRatio: image.size, insideRect: CGRect(origin: CGPoint.zero, size: size))
-        
-        // resize image
-//        let resizeImage = image.resizeImage(to: rec.size.width, height: rec.size.height)
-        
-//        print("new image size is \(resizeImage.size)")
-        guard let resizedImageData = image.jpegData(compressionQuality: 1.0) else {
-            return
-        }
-        
-        
-        // create an image object using the image selected
-        let imageObject = ImageObject(imageData: resizedImageData, date: Date())
-        
-        // insert new imageObject into imageObjects
-        imageObjects.insert(imageObject, at: 0)
-        
-        // create an indexPath for insertion into collection view
-        
-        let indexPath = IndexPath(row: 0, section: 0)
-        
-        // insert new cell into collection view
-        tableView.insertRows(at: [indexPath], with: .fade)
-//        collectionView.insertItems(at: [indexPath])
-        
-        // Persist imageObject to documents directory
-        do {
-            try dataPersistance.create(item: imageObject)
-        } catch {
-            print("saving error")
-        }
-    }
+//    func appendNewPhotoToCollection() {
+//        guard let image = selectedImage else {
+//            print("image is nil")
+//            return
+//        }
+//
+//        print("original image size is \(image.size)")
+//
+//        // the size for resizing of the image
+//        //        let size = UIScreen.main.bounds.size
+//        //        // we will maintain the aspect ratio of the image
+//        //        let rec = AVMakeRect(aspectRatio: image.size, insideRect: CGRect(origin: CGPoint.zero, size: size))
+//
+//        // resize image
+//        //        let resizeImage = image.resizeImage(to: rec.size.width, height: rec.size.height)
+//
+//        //        print("new image size is \(resizeImage.size)")
+//        guard let resizedImageData = image.jpegData(compressionQuality: 1.0) else {
+//            return
+//        }
+//
+//
+//        // create an image object using the image selected
+//        let imageObject = ImageObject(imageData: resizedImageData, date: Date())
+//
+//        // insert new imageObject into imageObjects
+//        imageObjects.insert(imageObject, at: 0)
+//
+//
+//        // create an indexPath for insertion into collection view
+//
+//        //        let indexPath = IndexPath(row: 0, section: 0)
+//
+//        // insert new cell into collection view
+//        //        tableView.insertRows(at: [indexPath], with: .fade)
+//        //        collectionView.insertItems(at: [indexPath])
+//
+//
+//
+//        // Persist imageObject to documents directory
+//        do {
+//            try dataPersistance.create(item: imageObject)
+//        } catch {
+//            print("saving error")
+//        }
+//    }
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
         print("dismiss")
@@ -117,35 +125,8 @@ class PostViewController: UIViewController {
         }
         present(imagePickerController, animated: true)
     }
-    
-    
-    
-    
 }
 
-extension PostViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return imageObjects.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "imageCell", for: indexPath) as? PhotoCell else {
-              fatalError("could not downcast to an ImageCell")
-          }
-          let images = imageObjects[indexPath.row]
-          cell.configuredCell(imageObject: images)
-          // step 5: creating custom delegation -> set delegate object
-          // similar to tableView.delegete = self
-//          cell.delegate = self
-          return cell
-    }
-}
-
-extension PostViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 300
-    }
-}
 
 extension  PostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
@@ -158,18 +139,18 @@ extension  PostViewController: UIImagePickerControllerDelegate, UINavigationCont
             print("Image selected not found")
             return
         }
-                selectedImage = image
+        photoLibraryImage.image = image
+//        selectedImage = image
         
         dismiss(animated: true)
     }
 }
 
-//extension UIImage {
-//    func resizeImage(to width: CGFloat, height: CGFloat) -> UIImage {
-//        let size = CGSize(width: width, height: height)
-//        let renderer = UIGraphicsImageRenderer(size: size)
-//        return renderer.image { (context) in
-//            self.draw(in: CGRect(origin: .zero, size: size))
-//        }
-//    }
-//}
+extension PostViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = ""
+           textView.textColor = UIColor.black
+        }
+    }
+}
